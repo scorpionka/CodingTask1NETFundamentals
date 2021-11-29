54c609758b4e3df54fab0379334523621e72cc5c
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2021-11-29 23:52:06 +0300

    Add project files.

---
+++ NETDiversityConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NETDiversityConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string userName;
+            userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}");
+        }
+    }
+}
